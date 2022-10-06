a76d138a0d9f615932a0936e7ec25feefc601695
aaravhatesrk <110599054+aaravhatesrk@users.noreply.github.com>
2022-10-06 15:47:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //https://teachablemachine.withgoogle.com/models/N2Q_hwIJ7/
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({ audio:true });
+    classifier= ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/N2Q_hwIJ7/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
